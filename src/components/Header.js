e5cc6bc1b0f03553876e0019bb478bf690834851
Dmitry <dimasya.qwerty2015@yandex.ru>
2021-03-04 17:22:34 +0300

    адаптив, меню бургер

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,23 @@ import { Link, Route, Switch } from 'react-router-dom';
 
 
 function Header(props) {
+  const [isBurgerActive, setIsBurgerActive] = React.useState(false);
+
+  function handleBurgerClick() {
+    setIsBurgerActive(!isBurgerActive);
+  }
 
   return (
     <header className="header">
-      <Link to="/" className="header__logo">
-        <img src={props.src} alt="Логотип с надписью Место." className="header__logo-img" />
-      </Link>
-      <div className="header__profile">
+      <div className="header__inner">
+        <Link to="/" className="header__logo">
+          <img src={props.src} alt="Логотип с надписью Место." className="header__logo-img" />
+        </Link>
+        <div className="header__burger" onClick={handleBurgerClick}>
+          <span className={`header__burger-line ${isBurgerActive && "header__burger-line_cross"}`}></span>
+        </div>
+      </div>
+      <div className={`header__profile ${!isBurgerActive && "header__profile_hidden"}`}>
         {props.loggedIn ? (<p className="header__email">{props.email}</p>) : ''}
         <Switch>
           <Route path="/sign-in">
